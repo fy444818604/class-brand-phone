dd9dae395201e41f701c9846efc7b4e3f370ed20
fy <444818604@qq.com>
2019-08-29 18:52:52 +0800

    付

--- js/mui.js
+++ js/mui.js
@@ -3041,13 +3041,12 @@ Function.prototype.bind = Function.prototype.bind || function(to) {
 		error: $.noop,
 		complete: $.noop,
 		context: null,
-		contentType: "application/x-www-form-urlencoded; charset=UTF-8",
 		xhr: function(protocol) {
 			return new window.XMLHttpRequest();
 		},
 		accepts: {
 			script: 'text/javascript, application/javascript, application/x-javascript',
-			json: 'application/json, text/javascript, */*; q=0.01',
+			json: jsonType,
 			xml: 'application/xml, text/xml',
 			html: htmlType,
 			text: 'text/plain'
